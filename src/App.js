52f7c7fc95a476527a50a562487960c9b5add51c
nishimura <nishimura@aivick.co.jp>
2024-09-02 14:17:52 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
-function App() {
+// 以下が認証機能のためのインポート文
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <View className="App">
+        <Card>
+          <Image src={logo} className="App-logo" alt="logo" />
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        // サインアウトボタンの追加
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
   );
 }
 
-export default App;
+// withAuthenticatorコンポーネントを利用
+export default withAuthenticator(App);
